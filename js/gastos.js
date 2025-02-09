6c80c8eef1d32be31bfd3f4f00f6309fb1439239
Diego Aranibar Ramos <aranibar.08diego@gmail.com>
2025-02-08 23:57:43 -0500

    se arregla

--- js/gastos.js
+++ js/gastos.js
@@ -1,18 +1,8 @@
 let current = null;
 $(document).ready(function () {
-    lista_usuarios();
+    lista_gastos();
     lista_sucursales();
-    lista_roles();
 });
-function lista_roles() {
-    $.post("ws/service.php?parAccion=lista_roles", function (response) {
-        var obj = JSON.parse(response);
-        $("#id_rol").append(`<option value="0">--SELECCIONE--</option>`);
-        $.each(obj, function (index, val) {
-            $("#id_rol").append(`<option value="${val.id}">${val.rol}</option>`);
-        });
-    });
-}
 function lista_sucursales() {
     $.post("ws/service.php?parAccion=lista_sucursales", function (response) {
         var obj = JSON.parse(response);
@@ -22,34 +12,30 @@ function lista_sucursales() {
         });
     });
 }
-function lista_usuarios() {
-    $.post("ws/service.php?parAccion=lista_usuarios", function (response) {
+function lista_gastos() {
+    $.post("ws/service.php?parAccion=lista_gastos", function (response) {
         var obj = JSON.parse(response);
-        $("#tabla-usuarios").find("tbody").empty();
+        $("#tabla-gastos").find("tbody").empty();
         $.each(obj, function (index, val) {
-            $("#tabla-usuarios").find("tbody").append(`<tr>
+            $("#tabla-gastos").find("tbody").append(`<tr>
                 <td>${val.id}</td>
-                <td>${val.nombres}</td>
-                <td>${val.dni}</td>
+                <td>${val.concepto}</td>
+                <td>${val.monto}</td>
                 <td>${val.sucursal}</td>
-                <td>${val.usuario}</td>
-                <td>${val.rol}</td>
+                <td>${val.fecha_creacion}</td>
                 <td>
-                    <span class="btn btn-outline-warning btn-sm d-block mb-1" data-toggle="modal" data-target="#formulario" onclick="editar_usuario(${val.id});"><i class="fa fa-edit"></i></span>
+                    <span class="btn btn-outline-warning btn-sm d-block mb-1" data-toggle="modal" data-target="#formulario" onclick="editar_gasto(${val.id});"><i class="fa fa-edit"></i></span>
                     <span  class="btn btn-outline-danger btn-sm d-block" onclick="eliminar_usuario(${val.id});"><i class="fa fa-trash"></i></span>
                 </td>
                 </tr>`);
         });
     });
 }
-function actualizar_usuario(id) {
-    $.post("ws/service.php?parAccion=actualizar_usuario", {
+function actualizar_gasto(id) {
+    $.post("ws/service.php?parAccion=actualizar_gasto", {
         id_sucursal: $("#id_sucursal").val(),
-        id_rol: $("#id_rol").val(),
-        nombres: $("#nombres").val(),
-        dni: $("#dni").val(),
-        usuario: $("#usuario").val(),
-        pass: $("#pass").val(),
+        concepto: $("#concepto").val(),
+        monto: $("#monto").val(),
         id: id,
         fecha_creacion: current.fecha_creacion,
         id_usuario_creacion: current.id_usuario_creacion
@@ -57,54 +43,49 @@ function actualizar_usuario(id) {
         var obj = JSON.parse(response);
         if (obj.Result == "OK") {
             alertify.success("Se modificó correctamente.");
-            lista_usuarios();
+            lista_gastos();
             limpiar_formulario();
         } else {
             alertify.error("Algo ha salido terriblemente mal.");
         }
     });
 }
-function insertar_usuario() {
-    $.post("ws/service.php?parAccion=insertar_usuario", {
+function insertar_gasto() {
+    $.post("ws/service.php?parAccion=insertar_gasto", {
         id_sucursal: $("#id_sucursal").val(),
-        id_rol: $("#id_rol").val(),
-        nombres: $("#nombres").val(),
-        dni: $("#dni").val(),
-        usuario: $("#usuario").val(),
-        pass: $("#pass").val(),
+        concepto: $("#concepto").val(),
+        monto: $("#monto").val()
     }, function (response) {
         var obj = JSON.parse(response);
         if (obj.Result == "OK") {
             alertify.success("Se agregó correctamente.");
-            lista_usuarios();
+            lista_gastos();
             limpiar_formulario();
         } else {
             alertify.error("Algo ha salido terriblemente mal.");
         }
     });
 }
-function nuevo_usuario() {
-    $("#usuarioModalLabel").text("Nuevo Usuario");
-    $("#btn-accion-usuario").text("Guardar");
-    $("#btn-accion-usuario").attr("onclick", "insertar_usuario();");
+function nuevo_gasto() {
+    $("#gastoModalLabel").text("Nuevo Gasto");
+    $("#btn-accion-gasto").text("Guardar");
+    $("#btn-accion-gasto").attr("onclick", "insertar_gasto();");
 }
-function editar_usuario(id) {
-    $("#usuarioModalLabel").text("Editar Usuario");
-    $("#btn-accion-usuario").text("Actualizar");
-    $("#btn-accion-usuario").attr("onclick", "actualizar_usuario(" + id + ");");
-    $.post("ws/service.php?parAccion=editar_usuario", {
+function editar_gasto(id) {
+    $("#gastoModalLabel").text("Editar Gasto");
+    $("#btn-accion-gasto").text("Actualizar");
+    $("#btn-accion-gasto").attr("onclick", "actualizar_gasto(" + id + ");");
+    $.post("ws/service.php?parAccion=editar_gasto", {
         id: id
     }, function (response) {
         var obj = JSON.parse(response);
         current = obj;
         $("#id_sucursal").val(obj.id_sucursal);
-        $("#nombres").val(obj.nombres);
-        $("#dni").val(obj.dni);
-        $("#usuario").val(obj.usuario);
-        $("#id_rol").val(obj.id_rol);
+        $("#concepto").val(obj.concepto);
+        $("#monto").val(obj.monto);
     });
 }
-function limpiar_formulario(){
+function limpiar_formulario() {
     $(".form-control").val('');
     $("select").val(0);
 }
@@ -116,7 +97,7 @@ function eliminar_usuario(id) {
             var obj = JSON.parse(response);
             if (obj.Result == "OK") {
                 alertify.success("Se eliminó correctamente.");
-                lista_usuarios();
+                lista_gastos();
             } else {
                 alertify.error("Algo ha salido terriblemente mal.");
             }
